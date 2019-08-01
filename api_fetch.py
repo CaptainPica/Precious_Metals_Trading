a58290df032586f1fcf90980f8f05a243762d801
CaptainPica <44632287+CaptainPica@users.noreply.github.com>
2019-08-01 16:07:46 -0500

    Slowly adding more get requests until heroku times out

--- api_fetch.py
+++ api_fetch.py
@@ -39,7 +39,11 @@ def harvest():
     # Sets the URLs in iterable list
     url_list = [CMEgold_futures,CMEgold_futures2,CMEsilver_futures,CMEsilver_futures2,CMEpalladium_futures,CMEpalladium_futures2,CMEplatinum_futures,CMEplatinum_futures2,CME_TenYear_futures,CME_TenYear_futures2,ICE_USD_futures,ICE_USD_futures2,ICE_ZAR_futures,ICE_ZAR_futures2,Gold_OpenInt,Silver_OpenInt,Palladium_OpenInt,Platinum_OpenInt]
     
-    info = get(url_list[0]).json()
+    # This is here to avoid the timeout
+    how_many = [0,1,2,3,4,5]
+    info_list = [get(url_list[i]).json() for i in how_many]
+    for i in how_many:
+        df_list[i].to_csv(f"data/csv/Data_Raw_{i}.csv")
     # # Gets the info in iterable list
     # info_list = [get(url).json() for url in url_list]
     # # Gets info in pd dataframes in iterable list
